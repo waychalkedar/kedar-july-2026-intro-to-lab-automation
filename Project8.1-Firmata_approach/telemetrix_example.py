db2e967dff6d83f275da3acd29666166859e6e63
waychalkedar <waychalkedar@gmail.com>
2026-07-29 14:26:04 +0300

    added telemetrix example python file

---
+++ Project8.1-Firmata_approach/telemetrix_example.py
@@ -0,0 +1,43 @@
+import sys
+import time
+
+from telemetrix import telemetrix
+
+board = telemetrix.Telemetrix()
+
+LED_PIN = 4
+BUTTON_PIN = 6
+
+# Callback data indices for telemetrix digital reporting
+CB_PIN_MODE = 0
+CB_PIN = 1
+CB_VALUE = 2
+CB_TIME = 3
+
+
+def button_callback(data):
+    """Handle button state changes and update the LED."""
+    pin = data[CB_PIN]
+    value = data[CB_VALUE]
+
+    if pin != BUTTON_PIN:
+        return
+
+    if value == 1:
+        board.digital_write(LED_PIN, 1)
+        print("Button pressed: LED on")
+    else:
+        board.digital_write(LED_PIN, 0)
+        print("Button released: LED off")
+
+
+board.set_pin_mode_digital_output(LED_PIN)
+board.set_pin_mode_digital_input(BUTTON_PIN, button_callback)
+board.enable_digital_reporting(BUTTON_PIN)
+
+try:
+    while True:
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    board.shutdown()
+    sys.exit(0)
