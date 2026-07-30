052b52a55fb06a1322066b25d396e3066aa65731
waychalkedar <waychalkedar@gmail.com>
2026-07-30 11:40:24 +0300

    added buzzer functionality

--- Project11-Final_Project_Part_1/servoAccel/servoAccel.ino
+++ Project11-Final_Project_Part_1/servoAccel/servoAccel.ino
@@ -1,37 +1,43 @@
+// modified with Copilot
+
 #include <Servo.h>
 #include "Arduino_SensorKit.h"
- 
+
+#define BUZZER 5
 const int SERVO_PIN = 7;
-int valueX;
+float valueX;
 int angle;
 
 Servo servo1;
 
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 void setup() {
   Serial.begin(9600);
   servo1.attach(SERVO_PIN);
+  pinMode(BUZZER, OUTPUT);
   while(!Serial);
   
   Accelerometer.begin();
 }
  
 void loop() {
-  // Read X-axis reading
-  
+  // Read accelerometer axis readings
   valueX = Accelerometer.readX();
-  Serial.print(valueX);
-  Serial.println("");
-  valueX = (valueX + 1) * 2.5;
-  if ((valueX > 0) & (valueX < 5)){
-    angle = analogRead(valueX);
-    angle = map(valueX, -1, 1, 0, 180);
-    Serial.print(angle);
-    Serial.println("");
+  // Map the raw X value to a servo angle.
+  // Adjust min/max values for your sensor orientation.
+  float mappedAngle = mapFloat(valueX, -1.0, 1.0, 0.0, 180.0);
+  // angle = constrain((int)mappedAngle, 0, 180);
+  // servo1.write(angle);
+  if ((valueX < -0.9) | (valueX > 0.9)){
+    tone(BUZZER, 440);
   }
   else {
-    Serial.println("Out of range");
+    noTone(BUZZER);
+    angle = constrain((int)mappedAngle, 0, 180);
+    servo1.write(angle);
   }
-  // valueY = Accelerometer.readY();
-  // servo1.write(angle);
-  delay(250);
+  delay(100);
 }
